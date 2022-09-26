a6fc5bff128061220412827d9a11ea851cf32524
bdonohue1986 <100311620+bdonohue1986@users.noreply.github.com>
2022-09-26 04:48:52 -0400

    initial commit

---
+++ TCproject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TCproject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Tavares";
+            int age = 36;
+            decimal weight = 148.9m;
+            double gpa = 3.7;
+            char t = 'T';
+            bool x=true;
+            if (x == false);
+            
+
+
+            Console.WriteLine($"Hello my name is {name} you can call me {t} for short. I/'am {age} weighing {weight} and my gpa was {gpa}. The real question is how will i do in this class? Well i take my time to fully understand the information ill be able to build whole programs Oh its {x} its damn {x} ");
+        }
+    }
+}
